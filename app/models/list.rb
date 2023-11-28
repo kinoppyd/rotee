d61154a832c091fb260f9825aa97d33c9fe1fb15
kinoppyd <ppyd@kinoppyd.dev>
2023-11-28 22:13:56 +0900

    Modify list view

--- app/models/list.rb
+++ app/models/list.rb
@@ -5,5 +5,15 @@ class List < ApplicationRecord
   validates :cycle, presence: true
   validates :next_trigger_at, presence: true
 
+  attribute :pointer, :integer, default: 0
+
   enum :cycle, { daily: 0, weekly: 1 }
+
+  before_validation :set_default_trigger
+
+  private
+
+  def set_default_trigger
+    self.next_trigger_at ||= Time.current + (daily? ? 1.day : 1.week)
+  end
 end
