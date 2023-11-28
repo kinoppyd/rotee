e5270154810d25eb17563c6940959d0cc2573989
kinoppyd <ppyd@kinoppyd.dev>
2023-11-28 21:31:11 +0900

    Add validates for list

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,9 @@
 class List < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :body, length: { maximum: 100 }
+  validates :pointer, presence: true
+  validates :cycle, presence: true
+  validates :next_trigger_at, presence: true
+
+  enum :cycle, { daily: 0, weekly: 1 }
 end
