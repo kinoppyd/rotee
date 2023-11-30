df04b080b88f35c897df02179e3a67bcbc88294d
kinoppyd <ppyd@kinoppyd.dev>
2023-11-30 10:10:43 +0900

    bin/rails g scaffold dashboard title:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_28_150726) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_30_011030) do
+  create_table "dashboards", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.integer "position"
