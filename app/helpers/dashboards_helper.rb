a344a64ef82646ae10feae7610eb2e82357a62fc
kinoppyd <ppyd@kinoppyd.dev>
2024-01-06 09:57:31 +0900

    Extracet turbo frame tags to helper

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,2 +1,11 @@
 module DashboardsHelper
+  # turbo frame tag id for new list in dashboard
+  def new_list_id(dashboard)
+    "new_list_in_#{dashboard.id}"
+  end
+
+  # Turbo frame tag for lists in dashboard
+  def dashboard_lists_id(dashboard)
+    "dashboard_lists_#{dashboard.id}"
+  end
 end
--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,16 @@
 module ListsHelper
+  # Turbo frame tag for new item in list
+  def new_item_id(list)
+    "new_item_in_#{list.id}"
+  end
+
+  # Turbo frame tag for items in list
+  def list_items_id(list)
+    "list_items_#{list.id}"
+  end
+
+  # Turbo frame tag for list element wrapper
+  def list_wrapper_id(list)
+    "list_#{list.id}_wrapper"
+  end
 end
