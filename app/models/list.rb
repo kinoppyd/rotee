be4e1df0c9d5fe0e90938d470f4184b684801307
kinoppyd <ppyd@kinoppyd.dev>
2024-01-08 12:18:07 +0900

    Use timer instead of cycle and next_trigger_at

--- app/models/list.rb
+++ app/models/list.rb
@@ -8,66 +8,20 @@ class List < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50 }
   validates :body, length: { maximum: 100 }
   validates :pointer, presence: true
-  validates :cycle, presence: true
-  validates :next_trigger_at, presence: true
   validates :dashboard_id, presence: true
 
   attribute :pointer, :integer, default: 0
-
-  enum :cycle, { daily: 0, weekly: 1 }
-
-  before_validation :set_default_trigger
-  before_update :set_updated_trigger
 
   def max_pointer
     items.size
   end
 
   def tick!(current = Time.current)
-    return if current < next_trigger_at
+    return if current < timer.next_tick_at
 
-    pointer_candidate = next_pointer(current)
+    pointer_candidate = timer.tick!
     self.pointer = max_pointer <= pointer_candidate ? pointer_candidate - max_pointer : pointer_candidate
 
-    self.next_trigger_at += (daily? ? 24.hours : (7 * 24).hours) while next_trigger_at >= current
     save!
-  end
-
-  private
-
-  def set_default_trigger
-    return if next_trigger_at_changed?
-
-    self.next_trigger_at ||= next_trigger
-  end
-
-  def set_updated_trigger
-    return if self.next_trigger_at_changed?
-
-    self.next_trigger_at = next_trigger
-  end
-
-  def next_trigger
-    today = Time.zone.today
-    if daily?
-      today = today.next_day.to_datetime
-    else
-      today = today.next_day until today.sunday?
-    end
-    today
-  end
-
-  def next_pointer(current)
-    return 0 if max_pointer.zero?
-
-    pointer + if current - next_trigger_at > threshold
-                ((current - next_trigger_at) / threshold).to_i % items.size
-              else
-                1
-              end
-  end
-
-  def threshold
-    @threshold ||= daily? ? 24 * 60 * 60 : 24 * 60 * 60 * 7
   end
 end
--- test/models/list_test.rb
+++ test/models/list_test.rb
@@ -6,76 +6,120 @@ class ListTest < ActiveSupport::TestCase
   end
 
   test "tick don't moves pointer when list is empty" do
-    list = List.create!(title: 'test', pointer: 0, cycle: 0, next_trigger_at: Time.current - 1.second, dashboard: @dashboard)
+    travel_to Date.new(2023, 12, 31) do
+      list = List.create!(title: 'test', pointer: 0, dashboard: @dashboard)
+      list.build_timer(trigger_day: Timer::DayOfWeekInteger::ALL_DAYS)
 
-    list.tick!
-    assert_equal 0, list.pointer
+      list.tick!
+      assert_equal 0, list.pointer
+    end
   end
 
   test "tick moves pointer forward 1 when over trigger_at 1 day" do
-    list = List.create!(title: 'test', pointer: 0, cycle: 0, next_trigger_at: Time.current - 1.second, dashboard: @dashboard)
+    list = List.create!(title: 'test', pointer: 0, dashboard: @dashboard)
     list.items.create!(name: 'item1', position: 0)
     list.items.create!(name: 'item2', position: 1)
 
-    list.tick!
-    assert_equal 1, list.pointer
+    travel_to Date.new(2023, 12, 31) do
+      list.build_timer(trigger_day: Timer::DayOfWeekInteger::ALL_DAYS).save!
+    end
+
+    travel_to Date.new(2024, 1, 1) do
+      list.tick!
+      assert_equal 1, list.pointer
+    end
   end
 
   test "tick moves pointer forward 2 when over trigger_at 2 days" do
-    list = List.create!(title: 'test', pointer: 0, cycle: 0, next_trigger_at: Time.current - 2.days, dashboard: @dashboard)
+    list = List.create!(title: 'test', pointer: 0, dashboard: @dashboard)
     list.items.create!(name: 'item1', position: 0)
     list.items.create!(name: 'item2', position: 1)
     list.items.create!(name: 'item3', position: 2)
 
-    list.tick!
-    assert_equal 2, list.pointer
+    travel_to Date.new(2023, 12, 31) do
+      list.build_timer(trigger_day: Timer::DayOfWeekInteger::ALL_DAYS).save!
+    end
+
+    travel_to Date.new(2024, 1, 2) do
+      list.tick!
+      assert_equal 2, list.pointer
+    end
   end
 
   test "tick moves pointer forward 2, but rollback to 0 when over trigger_at 2 days and list size is 2" do
-    list = List.create!(title: 'test', pointer: 0, cycle: 0, next_trigger_at: Time.current - 2.days, dashboard: @dashboard)
+    list = List.create!(title: 'test', pointer: 0, dashboard: @dashboard)
     list.items.create!(name: 'item1', position: 0)
     list.items.create!(name: 'item2', position: 1)
 
-    list.tick!
-    assert_equal 0, list.pointer
+    travel_to Date.new(2023, 12, 31) do
+      list.build_timer(trigger_day: Timer::DayOfWeekInteger::ALL_DAYS).save!
+    end
+
+    travel_to Date.new(2023, 1, 2) do
+      list.tick!
+      assert_equal 0, list.pointer
+    end
   end
 
   test "don't tick twice" do
-    list = List.create!(title: 'test', pointer: 0, cycle: 0, next_trigger_at: Time.current - 1.second, dashboard: @dashboard)
+    list = List.create!(title: 'test', pointer: 0, dashboard: @dashboard)
     list.items.create!(name: 'item1', position: 0)
     list.items.create!(name: 'item2', position: 1)
 
-    list.tick!
-    list.tick!
-    assert_equal 1, list.pointer
+    travel_to Date.new(2023, 12, 31) do
+      list.build_timer(trigger_day: Timer::DayOfWeekInteger::ALL_DAYS).save!
+    end
+
+    travel_to Date.new(2024, 1, 1) do
+      list.tick!
+      list.tick!
+      assert_equal 1, list.pointer
+    end
   end
 
   test "tick moves pointer forward 1 when over trigger_at 1 week" do
-    list = List.create!(title: 'test', pointer: 0, cycle: 1, next_trigger_at: Time.current - 7.days, dashboard: @dashboard)
+    list = List.create!(title: 'test', pointer: 0, dashboard: @dashboard)
     list.items.create!(name: 'item1', position: 0)
     list.items.create!(name: 'item2', position: 1)
 
-    list.tick!
-    assert_equal 1, list.pointer
+    travel_to Date.new(2023, 12, 31) do
+      list.build_timer(trigger_day: Timer::DayOfWeekInteger::SUN).save!
+    end
+
+    travel_to Date.new(2024, 1, 7) do
+      list.tick!
+      assert_equal 1, list.pointer
+    end
   end
 
   test "tick moves pointer forward 2 when over trigger_at 2 weeks" do
-    list = List.create!(title: 'test', pointer: 0, cycle: 1, next_trigger_at: Time.current - 14.days, dashboard: @dashboard)
+    list = List.create!(title: 'test', pointer: 0, dashboard: @dashboard)
     list.items.create!(name: 'item1', position: 0)
     list.items.create!(name: 'item2', position: 1)
     list.items.create!(name: 'item3', position: 2)
 
-    list.tick!
-    assert_equal 2, list.pointer
+    travel_to Date.new(2023, 12, 31) do
+      list.build_timer(trigger_day: Timer::DayOfWeekInteger::SUN).save!
+    end
+
+    travel_to Date.new(2024, 1, 14) do
+      list.tick!
+      assert_equal 2, list.pointer
+    end
   end
 
   test "tick moves pointer forward 2, but rollback to 0 when over trigger_at 2 weeks and list size is 2" do
-    list = List.create!(title: 'test', pointer: 0, cycle: 0, next_trigger_at: Time.current - 14.days, dashboard: @dashboard)
+    list = List.create!(title: 'test', pointer: 0, dashboard: @dashboard)
     list.items.create!(name: 'item1', position: 0)
     list.items.create!(name: 'item2', position: 1)
 
-    list.tick!
-    assert_equal 0, list.pointer
-  end
+    travel_to Date.new(2023, 12, 31) do
+      list.build_timer(trigger_day: Timer::DayOfWeekInteger::SUN).save!
+    end
 
+    travel_to Date.new(2024, 1, 14) do
+      list.tick!
+      assert_equal 0, list.pointer
+    end
+  end
 end
